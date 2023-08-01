c4c732f3e798f080b4a02eb90fe7011e8e181ba6
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-08-02 01:15:53 +0700

    [add]-multi user and checkrole middleware

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        if (Auth::user() && Auth::user()->role == 'admin') {
+            return redirect()->route('admin.dashboard');
+        } elseif (Auth::user() && Auth::user()->role == 'user') {
+            return redirect()->route('dashboard');
+        } else {
+            Auth::guard('web')->logout();
+            return redirect()->route('login')->with('status', 'Access Denied Bung!');
+        }
+
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
