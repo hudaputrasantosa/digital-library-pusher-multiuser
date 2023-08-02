adc2d8e3f60a7eb42ac19e5746322a6b92530d52
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-08-03 00:57:47 +0700

    [add]-loan book to user

---
+++ database/migrations/2023_08_02_162521_create_loans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id('id_loan');
+            $table->foreignId('id_book');
+            $table->foreignId('id_user');
+            $table->timestamps();
+
+            $table->foreign('id_book')->references('id_book')->on('books');
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
