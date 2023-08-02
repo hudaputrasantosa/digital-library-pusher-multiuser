1de7ea95787f58f0277dea94848119f695489c55
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-08-03 00:58:19 +0700

    [update]-add properti writer

--- database/migrations/2023_08_01_154433_create_books_table.php
+++ database/migrations/2023_08_01_154433_create_books_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id('id_book');
             $table->string('title');
+            $table->string('writer');
             $table->text('description');
             $table->string('cover');
             $table->string('file');
